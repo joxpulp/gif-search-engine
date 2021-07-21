a01fe74ab1e4b571ca648525a64357d8a0d99236
joxpulp <joxpulp@gmail.com>
2021-07-20 22:54:27 -0500

    minor optimizations and tests

--- src/tests/scenes/GifCard.test.js
+++ src/tests/scenes/GifCard.test.js
@@ -1,17 +1,13 @@
-import React from 'react'
+import React from 'react';
 import '@testing-library/jest-dom';
-import {shallow} from 'enzyme'
-import GifCard from '../../scenes/gifcard/GifCard'
+import { shallow } from 'enzyme';
+import GifCard from '../../scenes/gifcard/GifCard';
 
 describe('Pruebas componente <GifCard/>', () => {
-    
-    test('GifCard se renderiza correctamente', () => {
+	test('GifCard se renderiza correctamente', () => {
+		const url = 'https://localhost/gif.jpg';
+		const wrapper = shallow(<GifCard gifurl={url} />);
 
-        const url= 'https://localhost/gif.jpg'
-        const wrapper = shallow( <GifCard gifurl={url}/> )
-        
-        expect( wrapper ).toMatchSnapshot()
-        
-    })
-
-}) 
+		expect(wrapper).toMatchSnapshot();
+	});
+});
